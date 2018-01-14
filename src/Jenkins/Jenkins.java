ada4a683e1cff993694cb4a6c4e5e83115ad75aa
KNPENDYALA <K.N.Pendyala@gmail.com>
2018-01-13 19:49:58 -0500

    New Chnages

--- src/Jenkins/Jenkins.java
+++ src/Jenkins/Jenkins.java
@@ -9,7 +9,7 @@ public class Jenkins {
 	@Test
 	public void testJenkins() {
 	
-	System.out.println("Hello Kumar, Welcome To Jenkins..");
+	System.out.println("Hello, Welcome To Jenkins..");
 	
 	WebDriver driver;
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
