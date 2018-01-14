ef4d20e000be540b626a3a3a5c65505295a701bb
KNPENDYALA <K.N.Pendyala@gmail.com>
2018-01-13 19:15:58 -0500

    New Project for jenkins

---
+++ src/Jenkins/Jenkins.java
@@ -0,0 +1,21 @@
+package Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Jenkins {
+
+	@Test
+	public void testJenkins() {
+	
+	System.out.println("Hello, Welcome To Jenkins..");
+	
+	WebDriver driver;
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	driver= new ChromeDriver();
+	driver.get("http://www.bcbsm.com"); 
+	System.out.println(driver.getTitle());
+	driver.quit();
+}
+}
